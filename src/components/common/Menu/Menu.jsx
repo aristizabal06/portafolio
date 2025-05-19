8fcf5fcc300af29720ac86e1090a87747ee4c11a
Daniela Castañeda Aristizabal <arisdaniela871@gmail.com>
2025-05-19 14:06:45 -0500

    Se crea cambio final

--- src/components/common/Menu/Menu.jsx
+++ src/components/common/Menu/Menu.jsx
@@ -1,9 +1,45 @@
-export function Menu(){
+import { Link } from "react-router-dom"
 
-    return(
+import "./Menu.css"
+export function Menu() {
+
+    return (
 
         <>
-        
+            <nav class="navbar navbar-expand-lg manu ">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to="/danielacastañedaaristizabal">
+                    Daniela Castañeda Aristizabal
+                    </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/inicio">
+                                Inicio
+                            </Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link class="nav-link" to="/danielacastañedaaristizabal">
+                                Sobre mi
+                            </Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link class="nav-link" to="/proyectos">
+                                Mis Proyectos
+                            </Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link class="nav-link" aria-disabled="true" to="experiencia">
+                                Mi Experiencia
+                            </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
--- src/components/routes/Rutas/Rutas.jsx
+++ src/components/routes/Rutas/Rutas.jsx
@@ -3,6 +3,8 @@ import {Historia} from "../../pages/Historia/Historia"
 import {Introduccion} from "../../pages/Introduccion/Introduccion"
 import {Experiencia} from "../../pages/Experiencia/Experiencia"
 
+import { Menu } from "../../common/Menu/Menu"
+
 import { Routes, Route } from "react-router-dom"
 
 export function Rutas(){
@@ -10,10 +12,13 @@ export function Rutas(){
     return(
 
         <>
+
+          <Menu></Menu>
           <Routes>
+            <Route path="/" element={<Introduccion/>}></Route>
             <Route path="/inicio" element={<Home/>}></Route>
             <Route path="/proyectos" element={<Historia/>}></Route>
-            <Route path="/daniela" element={<Introduccion/>}></Route>
+            <Route path="/danielacastañedaaristizabal" element={<Introduccion/>}></Route>
             <Route path="/experiencia" element={<Experiencia/>}></Route>
           </Routes>
           
